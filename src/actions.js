27b712961b545a69df860f91b8a5c7d1a9952880
abethel912 <abethel180@gmail.com>
2023-03-22 18:24:59 -0400

    Connected actions to routes

---
+++ src/actions.js
@@ -0,0 +1,57 @@
+import { redirect } from 'react-router-dom'
+
+const URL = 'https://be-capstone.onrender.com'
+
+export const createAction = async ({ request }) => {
+  const formData = await request.formData()
+
+  const newActivity = {
+    name: formData.get('name'),
+    img: formData.get('img'),
+    state: formData.get('state'),
+    activityType: formData.get('activityType'),
+    description: formData.get('description'),
+    address: formData.get('address')
+  }
+
+  await fetch(URL + '/activities/', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newActivity)
+  })
+  return redirect('/')
+}
+
+export const updateAction = async ({ request, params }) => {
+  const formData = await request.formData()
+  const id = params.id
+  const updatedActivity = {
+    name: formData.get('name'),
+    img: formData.get('img'),
+    state: formData.get('state'),
+    activityType: formData.get('activityType'),
+    description: formData.get('description'),
+    address: formData.get('address')
+  }
+
+  let res = await fetch(URL + `/activities/${id}/`, {
+    method: 'put',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(updatedActivity)
+  })
+  console.log(res)
+  return redirect(`/${id}`)
+}
+
+export const deleteAction = async ({ params }) => {
+  const id = params.id
+  await fetch(URL + `/activities/${id}/`, {
+    method: 'delete'
+  })
+
+  return redirect('/')
+}
