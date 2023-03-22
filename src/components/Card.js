d3d4308813060fc19f2b424208f66370d9e923b1
abethel912 <abethel180@gmail.com>
2023-03-22 18:09:58 -0400

    Setup show page template

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,24 @@
-const Card = (props) => {
-  return <h1>Card</h1>
+import { Link } from 'react-router-dom'
+
+//destructure the post from props
+const Card = ({ card }) => {
+  //////////////////
+  // Style Objects
+  //////////////////
+  const div = {
+    textAlign: 'center',
+    border: '3px solid',
+    margin: '10px auto',
+    width: '80%'
+  }
+  return (
+    <div style={div}>
+      <Link to={`/post/${card.id}`}>
+        <h1>{card.subject}</h1>
+      </Link>
+      <h2>{card.details}</h2>
+    </div>
+  )
 }
 
 export default Card
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,28 @@
-const Show = (props) => {
-  return <h1>Show</h1>
+import { Link, useLoaderData } from 'react-router-dom'
+
+// destructuring the props needed to get our post, including router prop match
+const Show = () => {
+const card = useLoaderData()
+
+  ////////////////////
+  // Styles
+  ///////////////////
+  const div = {
+    textAlign: 'center',
+    border: '3px solid green',
+    width: '80%',
+    margin: '30px auto'
+  }
+
+  return (
+    <div style={div}>
+      <h1>{card.subject}</h1>
+      <h2>{card.details}</h2>
+      <Link to="/">
+        <button>Go Back</button>
+      </Link>
+    </div>
+  )
 }
 
 export default Show
