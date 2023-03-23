3bfcc752f6ca32e5bd1f358ec68a0cf635bb8e26
abethel912 <abethel180@gmail.com>
2023-03-22 20:43:52 -0400

    Need to fix navbar issue

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,10 +1,9 @@
-import { Link, useLoaderData } from 'react-router-dom'
-import Button from 'react-bootstrap/Button'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Link, useLoaderData, Form } from 'react-router-dom'
+import Navigation from '../components/Navigation'
 
 // destructuring the props needed to get our post, including router prop match
 const Show = () => {
-const card = useLoaderData()
+  const card = useLoaderData()
 
   ////////////////////
   // Styles
@@ -19,11 +18,54 @@ const card = useLoaderData()
   return (
     <div style={div}>
       <h1>{card.name}</h1>
-      <img src={card.img} alt="" />
-      <h2>{card.description}</h2>
-      <p>{card.address}</p>
+      <h2>{card.activityType}</h2>
+      <div style={{ textAlign: 'center' }}>
+        <Form action={`/update/${card.id}`} method="post">
+          <input
+            type="text"
+            name="name"
+            placeholder="recipe"
+            defaultValue={card.name}
+          />
+          <input
+            type="text"
+            name="img"
+            placeholder="image URL"
+            defaultValue={card.img}
+          />
+          <input
+            type="text"
+            name="cuisine"
+            placeholder="state"
+            defaultValue={card.state}
+          />
+          <input
+            type="text"
+            name="activityType"
+            placeholder="activity type"
+            defaultValue={card.activityType}
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="description"
+            defaultValue={card.description}
+          />
+          <input
+            type="text"
+            name="address"
+            placeholder="address"
+            defaultValue={card.address}
+          />
+          <button>Update Recipe</button>
+        </Form>
+        <Form action={`/delete/${card.id}`} method="post">
+          <button>Delete Recipe</button>
+          
+        </Form>
+      </div>
       <Link to="/">
-        <Button>Go Back</Button>
+        <button>Go Back</button>
       </Link>
     </div>
   )
