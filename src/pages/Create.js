1e0aa4e9a2fd5f3b14d3dd7a46e807b5c48827c5
abethel912 <abethel180@gmail.com>
2023-03-26 20:24:36 -0400

    Fixed Typo

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -7,16 +7,26 @@ function Create(props) {
       <div className="createcontainer">
         <div className="cardcreate">
           <div className="cardcontent">
-            <p className="cardtitle">Add a New Restaurant</p>
+            <p className="cardtitle">
+              <strong>Add a New Activity</strong></p>
             <Form action="/create" method="post" className="cardform">
-              <input type="text" name="name" placeholder="Activity Name" /> <br />
+              <input type="text" name="name" placeholder="Activity Name" />{' '}
+              <br />
               <input type="text" name="image" placeholder="Image URL" /> <br />
-              <input type="text" name="type" placeholder="Activity Type" /> <br />
-              <input type="text" name="description" placeholder="Description" /> <br />
-              <input type="text" name="address" placeholder="Address"
+              <input type="text" name="type" placeholder="Activity Type" />{' '}
+              <br />
+              <input
+                type="text"
+                name="description"
+                placeholder="Description"
               />{' '}
               <br />
-              <input type="submit" value="Add Activity" className="createsubmit"/>
+              <input type="text" name="address" placeholder="Address" /> <br />
+              <input
+                type="submit"
+                value="Add Activity"
+                className="createsubmit"
+              />
             </Form>
             <Link to="/">Home</Link>
           </div>
